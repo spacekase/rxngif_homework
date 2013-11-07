eefeb931a19a1d36bc2348f0d4b896dccf26a521
kasey lundquist <klundquist2014@kellogg.northwestern.edu>
2013-11-07 00:30:50 -0600

    Part 1-4

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,37 @@
 class PicturesController < ApplicationController
   def show
+  	@picture = Picture.find(params[:id])
   end
+
+  def index
+  	@list_of_pictures = Picture.all
+  end
+
+  def new
+
+  end
+
+  def create
+  	@picture = Picture.new
+  	@picture.caption = params[:caption]
+  	@picture.source = params[:source]
+  	@picture.save
+  end
+
+  def destroy
+  	@picture = Picture.find(params[:id])
+  	@picture.destroy
+  	@picture.save
+  end
+
+def edit
+	@edit_pic = Picture.find(params[:id])
+	@edit_pic.caption
+	@edit_pic.source
+end
+
+
+
+
+
 end
